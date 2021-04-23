0f5b21ba461ed91dcefdd9d160469864ee556ac6
Sazzad Hossain <sazzad.cse14@gmail.com>
2021-04-23 21:20:57 +0600

    new_task in work/task_queue

---
+++ work_queues/new_task.js
@@ -0,0 +1,36 @@
+// import the library
+var amqp = require('amqplib/callback_api');
+
+// connect to the RabbitMQ server
+amqp.connect('amqp://localhost', function(err, connection){
+    
+    if(err){
+        throw err;
+    }
+    // create a channel, which is where most of the API for getting things done resides
+    connection.createChannel(function(err1, channel){
+        if(err1){
+            throw err1;
+        }
+
+        // declare a queue 
+        var queue = 'task_queue';
+        var message = process.argv.slice(2).join(' ') || 'Hello world!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(message), {
+            persistent: true
+        });
+
+        console.log(' [x] sent %s', message); //The message content is a byte array, so you can encode whatever you like there.
+    });
+
+    //close the connection and exit
+    setTimeout(function(){
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
